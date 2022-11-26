23e845056cc2d717e2438d28cb98257b6658be7e
loiq.amonbekov <amonbekov.loiq@gmail.com>
2022-11-26 12:30:56 +0500

    Tendered card was added with input

--- resources/js/Pages/Point.vue
+++ resources/js/Pages/Point.vue
@@ -664,7 +664,9 @@
                     </div>
                     <div class="flex items-center">
                         <div class="text-lg text-white font-light">
-                            200TJS
+                            <div>
+                                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full" placeholder="Somoni" required>
+                            </div>
                         </div>
                     </div>
                 </div>
