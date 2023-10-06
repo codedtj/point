ec81c0964687afc4bdc450d2c658cfea8f2074f8
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2023-10-06 14:24:31 +0100

    move HasFactory to base model

--- src/app/Models/Point.php
+++ src/app/Models/Point.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Point extends Model
 {
-    use HasFactory;
-
     public function consignmentNotes(): HasMany
     {
         return $this->hasMany(ConsignmentNote::class);
--- src/app/Models/Role.php
+++ src/app/Models/Role.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\Relations\Relation;
 
 class Role extends Model
 {
-    use HasFactory;
-
     protected static function boot()
     {
         parent::boot();
