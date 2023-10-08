5d80f561151fc5c8fa445da45b679aa4114f238b
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2023-10-08 13:05:59 +0100

    return categories and price

--- src/app/Http/Controllers/API/Item/ItemController.php
+++ src/app/Http/Controllers/API/Item/ItemController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\API\Item;
 
 use App\Http\Controllers\API\ApiController;
+use App\Models\ConsignmentNote;
 use App\Models\Item;
 
 class ItemController extends ApiController
 {
     public function show(Item $item): Item
     {
+        $item->load('category');
+        $item->price = ConsignmentNote::query()->latest()->whereHas('items', function ($query) use ($item) {
+            $query->where('item_id', $item->id);
+        })->first()->price ?? 0;
         return $item;
     }
 }
--- src/app/Models/Item.php
+++ src/app/Models/Item.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @property string $id
+ */
 class Item extends Model
 {
     public function category(): BelongsTo
