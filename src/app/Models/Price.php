b154b08d66922583be495c4adac1a1f99b3fe282
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2023-11-04 23:07:23 +0000

    add price model and relations

---
+++ src/app/Models/Price.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property string $id
+ * @property double $base
+ * @property ?double $manual
+ * @property string $item_id
+ * @property string $point_id
+ * @property Item $item
+ * @property Point $point
+ */
+class Price extends Model
+{
+    protected $casts = [
+        'base' => 'double',
+        'final' => 'double',
+    ];
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function point(): BelongsTo
+    {
+        return $this->belongsTo(Point::class);
+    }
+}
---
+++ src/database/migrations/2023_11_04_223715_create_prices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->double('base');
+            $table->double('manual')->nullable();
+            $table->foreignUuid('item_id')->index();
+            $table->foreignUuid('point_id')->index();
+            $table->timestamps();
+
+            // Define foreign keys to establish relationships with "item" and "point" models
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->foreign('point_id')->references('id')->on('points');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prices');
+    }
+};
