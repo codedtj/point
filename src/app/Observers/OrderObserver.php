1da6fcac842a45c2f768de712782181108b83a61
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2023-11-04 17:24:23 +0000

    send email to boo

---
+++ src/app/Observers/OrderObserver.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Observers;
+
+use App\Mail\OrderCreated;
+use App\Models\Order;
+use Illuminate\Support\Facades\Mail;
+
+class OrderObserver
+{
+    public function created(Order $order): void
+    {
+        Mail::to('info@boo.tj')->send(new OrderCreated($order));
+    }
+
+    public function updated(Order $order): void
+    {
+        //
+    }
+
+    public function deleted(Order $order): void
+    {
+        //
+    }
+
+    public function restored(Order $order): void
+    {
+        //
+    }
+
+    public function forceDeleted(Order $order): void
+    {
+        //
+    }
+}
---
+++ src/resources/views/emails/orders/created.blade.php
@@ -0,0 +1,4 @@
+<body class="font-sans antialiased">
+<h1>{{__("New Order Created")}}</h1>
+<a href="">{{__("View order") . " " . $order->code}}</a>
+</body>
