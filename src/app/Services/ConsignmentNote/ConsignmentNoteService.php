babf70d61f4620ec5ddb71fd3edcd65e8b759020
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2023-11-19 14:24:24 +0000

    refactorConsignmentNoteService

--- src/app/Services/ConsignmentNote/ConsignmentNoteService.php
+++ src/app/Services/ConsignmentNote/ConsignmentNoteService.php
@@ -14,51 +14,67 @@ class ConsignmentNoteService
 {
     public function createFromOrder(Order $order, ConsignmentNoteStatus $status): ConsignmentNote
     {
-        /** @var ConsignmentNote $consignmentNote */
-        $consignmentNote = ConsignmentNote::query()->create([
+        /** @var ConsignmentNote */
+        return ConsignmentNote::query()->create([
             'point_id' => $order->point_id,
             'status' => $status,
             'type' => ConsignmentNoteType::Out
         ]);
-
-        return $consignmentNote;
     }
 
     public function processItemStockAndPriceChange(ConsignmentNote $consignmentNote): void
     {
         foreach ($consignmentNote->items as $item) {
-            DB::transaction(function () use ($item, $consignmentNote) {
-                /** @var StockBalance $balance */
-                $balance = StockBalance::query()->firstOrCreate(
-                        [
-                            'item_id' => $item->id,
-                            'point_id' => $consignmentNote->point_id,
-                        ], [
-                            'item_id' => $item->id,
-                            'point_id' => $consignmentNote->point_id,
-                            'quantity' => 0,
-                        ]
-                    );
-
-                $newQuantity = match ($consignmentNote->type) {
-                    ConsignmentNoteType::In => $balance->quantity + $item->pivot->quantity,
-                    ConsignmentNoteType::Out, ConsignmentNoteType::Transfer => $balance->quantity - $item->pivot->quantity,
-                };
-
-                /** @var Price $oldPrice */
-                $oldPrice = Price::query()->where('item_id', $item->id)->first();
-
-                if (!$oldPrice?->manual && $oldPrice?->base !== $item->pivot->price) {
-                    Price::query()->create([
-                            'item_id' => $item->id,
-                            'base' => $item->pivot->price,
-                        ]);
-                }
-
-                $balance->update([
-                    'quantity' => $newQuantity,
-                ]);
-            }, 3);
+            DB::transaction(fn () => $this->processSingleItem($item, $consignmentNote), 3);
         }
+    }
+
+    private function processSingleItem($item, ConsignmentNote $consignmentNote): void
+    {
+        $balance = $this->updateStockBalance($item, $consignmentNote);
+        $this->updatePriceIfNeeded($item);
+        $this->finalizeStockBalanceUpdate($balance, $item, $consignmentNote);
+    }
+
+    private function updateStockBalance($item, ConsignmentNote $consignmentNote): StockBalance
+    {
+        /** @var StockBalance */
+        return StockBalance::query()->firstOrCreate(
+            [
+                'item_id' => $item->id,
+                'point_id' => $consignmentNote->point_id,
+            ],
+            [
+                'item_id' => $item->id,
+                'point_id' => $consignmentNote->point_id,
+                'quantity' => 0,
+            ]
+        );
+    }
+
+    private function updatePriceIfNeeded($item): void
+    {
+        /** @var Price $oldPrice*/
+        $oldPrice = Price::query()->where('item_id', $item->id)->first();
+        if (!$oldPrice?->manual && $oldPrice?->base !== $item->pivot->price) {
+            Price::query()->create([
+                'item_id' => $item->id,
+                'base' => $item->pivot->price,
+            ]);
+        }
+    }
+
+    private function finalizeStockBalanceUpdate(StockBalance $balance, $item, ConsignmentNote $consignmentNote): void
+    {
+        $newQuantity = $this->calculateNewQuantity($balance, $item, $consignmentNote);
+        $balance->update(['quantity' => $newQuantity]);
+    }
+
+    private function calculateNewQuantity(StockBalance $balance, $item, ConsignmentNote $consignmentNote): int
+    {
+        return match ($consignmentNote->type) {
+            ConsignmentNoteType::In => $balance->quantity + $item->pivot->quantity,
+            ConsignmentNoteType::Out, ConsignmentNoteType::Transfer => $balance->quantity - $item->pivot->quantity,
+        };
     }
 }
