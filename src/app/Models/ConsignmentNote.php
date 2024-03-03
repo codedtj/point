55e6744164c72e7f4a494e916c986f6863a23f14
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2024-03-03 13:09:09 +0000

    add destinationPoint relation

--- src/app/Models/ConsignmentNote.php
+++ src/app/Models/ConsignmentNote.php
@@ -11,10 +11,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 /**
  * @property string $id
  * @property string $point_id
+ * @property ?string destination_point_id
  * @property ConsignmentNoteStatus $status
  * @property ConsignmentNoteType $type
  * @property Collection<Item> $items
  * @property Point $point
+ * @property Point $destinationPoint
  */
 class ConsignmentNote extends Model
 {
@@ -24,6 +26,11 @@ class ConsignmentNote extends Model
     ];
 
     public function point(): BelongsTo
+    {
+        return $this->belongsTo(Point::class);
+    }
+
+    public function destinationPoint(): BelongsTo
     {
         return $this->belongsTo(Point::class);
     }
